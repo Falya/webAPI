a7a0bd5abb944d742b4112f6fc565457018aa515
Maksim <anger65@gmail.com>
2019-06-11 14:15:30 +0300

    Delete config.js

--- src/config/config.js
+++
@@ -1,6 +0,0 @@
-const DB_CONFIG = {
-  connectionUrl: 'mongodb+srv://root:root@cinemacluster-fmgmj.mongodb.net/test?retryWrites=true',
-  dbName: 'CinemaDb'
-}
-
-module.exports.DB_CONFIG = DB_CONFIG;
