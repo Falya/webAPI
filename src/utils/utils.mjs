d28fb15a7f5856ebcb6e399f55036e1db9f30265
Maksim <anger65@gmail.com>
2019-07-16 15:08:23 +0300

    Fixed comments Refactored filter method Change some code style

--- src/utils/utils.mjs
+++ src/utils/utils.mjs
@@ -1,54 +1,59 @@
-export function filterSeancesByFeatures(seances, emptyAmount, hasEmptyVip, hasEmptyDouble) {
-  let filterList = [];
-  emptyAmount && filterList.push(emptySeatsFilter);
-  hasEmptyVip && filterList.push(emptyVipFilter);
-  hasEmptyDouble && filterList.push(emptyDoubleFilter);
+export function filterSeancesByFeatures(features) {
+  const { hasEmptyMoreOne, hasEmptyMoreTwo, hasEmptyVip, hasEmptyDouble } = features;
+  const emptyAmount = hasEmptyMoreTwo ? 2 : hasEmptyMoreOne ? 1 : 0;
+  const params = [
+    {
+      status: !!emptyAmount,
+    },
+    {
+      rowType: 'vip',
+      status: hasEmptyVip,
+    },
+    {
+      rowType: 'double',
+      status: hasEmptyDouble,
+    },
+  ];
 
-  return filterList.reduce((acc, filter) => {
-    acc = filter(acc, emptyAmount);
-    return acc;
-  }, seances);
+  const filterList = params.filter(filter => filter.status === true).map(rowType => setFilter(rowType, emptyAmount));
+
+  return seances => filterList.reduce((acc, filter) => filter(acc), seances);
 }
 
-function emptyVipFilter(seances, emptyAmount) {
-  return seances.filter(seance => {
-    let soldSeats = [];
-    const emptySeats =
-      seance.hallId.rows.reduce((sum, row) => {
-        if (row.rowType === 'vip') {
-          sum += row.rowLength;
-          const currentRow = seance.soldSeats.filter(({ rowNumber }) => rowNumber === row.rowNumber);
-          soldSeats = [...soldSeats, ...currentRow];
-        }
-        return sum;
-      }, 0) - soldSeats.length;
-    return emptySeats > emptyAmount;
-  });
+export function video3dFilter(hasVideo3d, query) {
+  hasVideo3d && query.where({ 'format.video': '3D' });
+  return query;
 }
 
-function emptyDoubleFilter(seances, emptyAmount) {
-  return seances.filter(seance => {
-    let soldSeats = [];
-    const emptySeats =
-      seance.hallId.rows.reduce((sum, row) => {
-        if (row.rowType === 'double') {
-          sum += row.rowLength;
-          const currentRow = seance.soldSeats.filter(({ rowNumber }) => rowNumber === row.rowNumber);
-          soldSeats = [...soldSeats, ...currentRow];
-        }
-        return sum;
-      }, 0) - soldSeats.length;
-    return emptySeats > emptyAmount;
-  });
+function setFilter(rowType = null, emptyAmount) {
+  return rowType ? emptyTypedFilter(rowType, emptyAmount) : emptySeatsFilter(emptyAmount);
 }
 
-function emptySeatsFilter(seances, emptyAmount) {
-  return seances.filter(seance => {
-    const emptySeats =
-      seance.hallId.rows.reduce((sum, row) => {
-        sum += row.rowLength;
-        return sum;
-      }, 0) - seance.soldSeats.length;
-    return emptySeats > emptyAmount;
-  });
+function emptyTypedFilter(rowType, emptyAmount) {
+  return seances => {
+    return seances.filter(seance => {
+      const seatsSum = seance.hallId.rows.reduce(
+        (sum, row) => {
+          if (rowType && row.rowType === rowType) {
+            sum.allSeats += row.rowLength;
+            const currentRow = seance.soldSeats.filter(({ rowNumber }) => rowNumber === row.rowNumber);
+            sum.soldSeats += currentRow.length;
+          }
+          return sum;
+        },
+        { soldSeats: 0, allSeats: 0 }
+      );
+      const emptySeats = seatsSum.allSeats - seatsSum.soldSeats;
+      return emptySeats > emptyAmount;
+    });
+  };
+}
+
+function emptySeatsFilter(emptyAmount) {
+  return seances => {
+    return seances.filter(seance => {
+      const emptySeats = seance.hallId.rows.reduce((sum, row) => (sum += row.rowLength), 0) - seance.soldSeats.length;
+      return emptySeats > emptyAmount;
+    });
+  };
 }
